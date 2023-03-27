9979ea51b0946f4f5ebb99eee3223df377937fcf
Vitor Diniz <dinizadroit@gmail.com>
2023-03-27 14:48:20 -0300

    React Components

---
+++ src/components/HelloWorld/index.tsx
@@ -0,0 +1,11 @@
+interface HelloWorldProps {
+  text: string;
+}
+
+export default function HelloWorld({ text }: HelloWorldProps) {
+  return <>{text}</>;
+}
+
+HelloWorld.defaultProps = {
+  text: "Hello World",
+};
