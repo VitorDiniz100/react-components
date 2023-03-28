146b7ccd63ac224d7759844aff38cb2a44b14d22
Vitor Diniz <dinizadroit@gmail.com>
2023-03-28 14:12:02 -0300

    multiple

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      <Accordion.Root type="single">
+      <Accordion.Root type="multiple">
         <Accordion.Item title="Informações" icon={{ element: <Trash /> }}>
           <p>Oi</p>
         </Accordion.Item>
