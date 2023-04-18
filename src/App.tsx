9dcbc3ba5fe1342dc94db34d87184ceebdc07fa0
Vitor Diniz <dinizadroit@gmail.com>
2023-04-18 11:28:36 -0300

    app

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ const appStyles: CSSProperties = { width: '1440px', margin: '0 auto' }
 export default function App() {
   return (
     <div className="App" style={appStyles}>
-      <p>f</p>
       <AccordionExample />
     </div>
   )
@@ -32,10 +31,12 @@ const AccordionExample = () => {
             activeChildren: <Minus />,
           }}
         >
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum fugiat
-          temporibus numquam tempore esse, ipsam praesentium autem odio nostrum.
-          Vitae veniam totam obcaecati repudiandae doloribus similique possimus,
-          quis aspernatur nisi.
+          <div>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum
+            fugiat temporibus numquam tempore esse, ipsam praesentium autem odio
+            nostrum. Vitae veniam totam obcaecati repudiandae doloribus
+            similique possimus, quis aspernatur nisi.
+          </div>
         </Accordion.Item>
         <Accordion.Item
           title="Lorem 2"
@@ -45,10 +46,12 @@ const AccordionExample = () => {
             activeChildren: <Minus />,
           }}
         >
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum fugiat
-          temporibus numquam tempore esse, ipsam praesentium autem odio nostrum.
-          Vitae veniam totam obcaecati repudiandae doloribus similique possimus,
-          quis aspernatur nisi.
+          <div>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum
+            fugiat temporibus numquam tempore esse, ipsam praesentium autem odio
+            nostrum. Vitae veniam totam obcaecati repudiandae doloribus
+            similique possimus, quis aspernatur nisi.
+          </div>
         </Accordion.Item>
         <Accordion.Item
           title="Lorem 3"
